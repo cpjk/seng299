2b6b15495df9ab52a18aa1f436e592f519962551
Chris Kelly <cpjkelly@gmail.com>
2015-07-12 12:00:32 -0700

    Add bookable to booking

--- app/models/booking.js
+++ app/models/booking.js
@@ -2,6 +2,7 @@ var mongoose = require('../../app/models/mongoose');
 
 var bookingSchema = mongoose.Schema({
   timeSlots: [{type: mongoose.Schema.Types.ObjectId, ref: 'TimeSlot'}],
+  bookable: {type: mongoose.Schema.Types.ObjectId, ref: 'Bookable'},
   date: Date
 });
 

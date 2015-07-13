c2219fd18a8f52e2f66b775fc30d4852d55176af
Chris Kelly <cpjkelly@gmail.com>
2015-07-13 01:39:29 -0700

    Distinguish booked timeSlots from unboooked

--- public/app/services/bookingService.js
+++ public/app/services/bookingService.js
@@ -6,6 +6,10 @@ angular.module('bookingService', [])
     return $http.get('/api/bookings');
   };
 
+  factory.bookedTimeSlots = function(bookables){
+    return $http.post('/api/bookings/time_slots_booked', {bookables: bookables});
+  };
+
   factory.create = function(booking){
     return $http.post('/api/bookings', booking);
   };
